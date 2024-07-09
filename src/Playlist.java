7c22edd3d2dbe51a2d908b1b50374179cab0c3f6
kazu-sugi-138 <147585119+kazu-sugi-138@users.noreply.github.com>
2024-07-09 14:12:52 +0900

    頭の部分も更新しました(7/9更新)

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,143 +1,47 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Playlist{
 
-	public static void main(String[] args) {
-		
-		//ここから先は他のクラスから取得するデータ
-		
-		int N=100;			//取得する曲数　100固定の予定
-		int maxtime=6000;	//再生リストの時間　0.95倍から1.05倍の間で出来上がる
-		int condition=4;	//1:再生回数　2:曲数多い　3:曲数少ない　4:時間ピッタリ
-		
-		setArray list=new setArray(N,(int)((maxtime)*1.026+20));	//コンストラクタ呼びだし
-		
-		list.add("cross a line",325,2881264,"id");
-		list.add("ミセナイクセニ",200,118488,"id");
-		list.add("夜は仄か",198,28009356,"id");
-		list.add("太陽のロロロロロノウェ",138,19464,"id");
-		list.add("あの子は悪魔",183,342070,"id");
-		list.add("目に緋色",180,513055,"id");
-		list.add("半透明",197,392630,"id");
-		list.add("藍才",274,15110928,"id");
-		list.add("さよーならまたいつか",208,32156409,"id");
-		list.add("?",130,10382,"id");
-		list.add("群青参加",260,25146078,"id");
-		list.add("ドライフラワー",287,180777,"id");
-		list.add("ゆうせいむぼう",262,24147944,"id");
-		list.add("神下り",223,387698,"id");
-		list.add("いっと",130,240567,"id");
-		list.add("いがく",120,17893640,"id");
-		list.add("framingo",217,182786985,"id");
-		list.add("人マニア",127,31447352,"id");
-		list.add("感電",293,226984687,"id");
-		list.add("冒険六",229,3070913,"id");
-		list.add("take a picture",225,91181165,"id");
-		list.add("星を仰ぐ",299,28128264,"id");
-		list.add("w/x/y",279,153938352,"id");
-		list.add("群青ランナウェイ",239,15784028,"id");
-		list.add("愛を知るまでは",278,23184715,"id");
-		list.add("一途",190,110470612,"id");
-		list.add("ごめんね",274,14479577,"id");
-		list.add("なにこれ",198,15479708,"id");
-		list.add("chopstick",176,46298043,"id");
-		list.add("弱音は奇",168,63743517,"id");
-		list.add("怪物",208,358769857,"id");
-		list.add("残響参加",181,221736869,"id");
-		list.add("踊",209,205544095,"id");
-		list.add("ギラギラ",277,171910112,"id");
-		list.add("cry baby",240,151989153,"id");
-		list.add("ただ声一つ",162,130656360,"id");
-		list.add("きらり",240,125634398,"id");
-		list.add("シンデレラボーイ",244,108627943,"id");
-		list.add("初心LOVE",228,106414876,"id");
-		list.add("群青",361,106313595,"id");
-		list.add("大統領になったらね",194,98424082,"id");
-		list.add("春泥棒",299,96141780,"id");
-		list.add("阿修羅ちゃん",198,93874736,"id");
-		list.add("踊り子",245,91717976,"id");
-		list.add("one last kiss",258,89246514,"id");
-		list.add("三原色",226,85074289,"id");
-		list.add("好きだから",298,81267162,"id");
-		list.add("ヴァンパイア",180,79964405,"id");
-		list.add("アンコール",281,77052347,"id");
-		list.add("忘れ",272,76335222,"id");
-		list.add("神っぽいな",204,73769161,"id");
-		list.add("paleblue",321,71574060,"id");
-		list.add("ペテルギウス",270,67723379,"id");
-		list.add("おもかげ",244,65362727,"id");
-		list.add("poppin shakin",197,64641355,"id");
-		list.add("U",205,59060824,"id");
-		list.add("フォニ",188,57668278,"id");
-		list.add("恋古月夜に",203,56958636,"id");
-		list.add("暗く宇六",258,56550045,"id");
-		list.add("選考",309,56085212,"id");
-		list.add("hellohello",195,53803609,"id");
-		list.add("シンデレラガール",208,53717066,"id");
-		list.add("なんでもないよ",270,49248185,"id");
-		list.add("夜の人笑い",240,49005598,"id");
-		list.add("chopstick",176,46308414,"id");
-		list.add("エゴロック",171,45488504,"id");
-		list.add("ラブレター",209,45292799,"id");
-		list.add("ロウワー",230,44960061,"id");
-		list.add("boy",244,44871229,"id");
-		list.add("のびしろ",219,44265565,"id");
-		list.add("より良い",217,44044011,"id");
-		list.add("死神",119,42582648,"id");
-		list.add("breakfast",191,42258541,"id");
-		list.add("plastic love",309,41662071,"id");
-		list.add("ki wasurai",230,41501206,"id");
-		list.add("なにやってもうまくいかない",147,41017231,"id");
-		list.add("解凍",203,40347048,"id");
-		list.add("君に夢中",261,40074519,"id");
-		list.add("不思議",353,39505322,"id");
-		list.add("ちんてんか",230,38712653,"id");
-		list.add("shining one",258,38076488,"id");
-		list.add("花占い",236,37595376,"id");
-		list.add("paradise",328,37572211,"id");
-		list.add("キュートな彼女",131,37360453,"id");
-		list.add("one ok",247,36917639,"id");
-		list.add("涙の海を超えて行け",241,36894853,"id");
-		list.add("curtain",266,36883473,"id");
-		list.add("優しい水星",219,35990124,"id");
-		list.add("小悪魔だってかまわない",209,35479700,"id");
-		list.add("開会来たん",224,34814843,"id");
-		list.add("メンタルチェンソー",177,34384277,"id");
-		list.add("明星",273,34275700,"id");
-		list.add("家庭の事情",236,32583146,"id");
-		list.add("daice",323,32336069,"id");
-		list.add("evolution",226,32069957,"id");
-		list.add("fakeland",216,31950353,"id");
-		list.add("yumyumyum",172,31917162,"id");
-		list.add("universe",334,31175325,"id");
-		list.add("easygame",185,30987587,"id");
-		list.add("サクラが降る夜は",292,30627626,"id");		//曲タイトル 時間 再生回数 URL
-		
-		//ここまでが他のクラスから取得するデータ
-		list.resetting(maxtime);
+	public static ArrayList<ArrayList<Object>> processAndSort(List<List<Object>> videoData, int time, int optimizationCondition) {
+        int N = videoData.size();
+        int maxtime = time;
+
+        setArray list = new setArray(N, (int)((maxtime+1)*1.026+20));
+
+        for (List<Object> video : videoData) {
+            String title = (String) video.get(0);
+            int videoTime = (int) video.get(1);
+            int viewCount = (int) video.get(2);
+            String videoId = (String) video.get(3);
+            list.add(title, videoTime, viewCount, videoId);
+        }
+
+        list.resetting(maxtime);
 		N=list.recount();
 		list.rearray(N,(int)((maxtime+1)*1.05));
-		switch(condition) {
+		switch(optimizationCondition) {
 			case 1:
 				list.sort1(maxtime);						//再生回数ソート
-				list.arrayupdate(maxtime, N, condition);
+				list.arrayupdate(maxtime, N, optimizationCondition);
 				break;
 			case 2:
 				list.sort2(maxtime);						//曲数多いソート
-				list.arrayupdate(maxtime, N, condition);
+				list.arrayupdate(maxtime, N, optimizationCondition);
 				break;
 			case 3:
 				list.sort3(maxtime);						//曲数少ないソート
-				list.arrayupdate(maxtime, N, condition);
+				list.arrayupdate(maxtime, N, optimizationCondition);
 				break;
 			case 4:
 				list.sort4(maxtime);						//時間ピッタリソート
-				list.arrayupdate(maxtime, N, condition);
+				list.arrayupdate(maxtime, N, optimizationCondition);
 				break;
 		}
 		list.delete();			//不必要な曲を削除
 		list.print();	//出力
-	}
+        return setArray.Arraylist;
+    }
 }
 
 
