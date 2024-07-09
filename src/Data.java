cc75a0b8085336cd537fe16cefc255001254c3f7
mizukikurage <148203915+mizukikurage@users.noreply.github.com>
2024-07-09 11:02:14 +0900

    Update Data.java

--- src/Data.java
+++ src/Data.java
@@ -1,18 +1,30 @@
 package com.example.youtubeapi;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.YouTubeRequestInitializer;
+import com.google.api.services.youtube.YouTubeScopes;
+import com.google.api.services.youtube.model.Playlist;
+import com.google.api.services.youtube.model.PlaylistSnippet;
+import com.google.api.services.youtube.model.PlaylistStatus;
 import com.google.api.services.youtube.model.Video;
 import com.google.api.services.youtube.model.VideoListResponse;
 
@@ -20,7 +32,8 @@ public class Data {
 
     private static final String APPLICATION_NAME = "youtube-api-example";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String API_KEY = "AIzaSyC4IFYlzBbU9JJxiRebO3g0nhTl5eIt8Oo"; // 作成したAPIキーをここに入力
+    private static final List<String> SCOPES = Arrays.asList(YouTubeScopes.YOUTUBE);
+    private static final String CREDENTIALS_FILE_PATH = "/client_secrets.json";
 
     public static List<List<Object>> processSearchWord(String searchWord) {
         List<List<Object>> allvideo = new ArrayList<>();
@@ -45,6 +58,13 @@ public class Data {
                     .setId(String.join(",", videoIds));
             VideoListResponse videoResponse = videosRequest.execute();
             List<Video> videoList = videoResponse.getItems();
+            
+            // プレイリストを作成
+            String playlistId = createPlaylist(youtubeService, "New Playlist", "A playlist created using the YouTube Data API");
+            System.out.println("Created Playlist ID: " + playlistId);
+            String playListURL = "Playlist URL: https://www.youtube.com/playlist?list=" + playlistId;
+            System.out.println(playListURL);
+            
 
             // 動画の詳細情報を保存
             for (Video video : videoList) {
@@ -53,20 +73,25 @@ public class Data {
                 String duration = video.getContentDetails().getDuration();
                 BigInteger viewCount = video.getStatistics().getViewCount();
                 String videoId = video.getId();
-                
+
                 // 時間を秒単位に変換
                 Duration duration1 = Duration.parse(duration);
                 int seconds = (int) duration1.getSeconds();
-                
-                //型を変換
+
+                // 型を変換
                 int counts = viewCount.intValue();
 
-                videoData.add((String) title);
-                videoData.add((int) seconds);
-                videoData.add((int) counts);
-                videoData.add((String) videoId);
+                // Listに入れ込む
+                videoData.add(title);
+                videoData.add(seconds);
+                videoData.add(counts);
+                videoData.add(videoId);
+                
                 allvideo.add(videoData);
+                
+                
             }
+
         } catch (GeneralSecurityException | IOException e) {
             e.printStackTrace();
         }
@@ -74,23 +99,64 @@ public class Data {
     }
 
     private static YouTube getService() throws GeneralSecurityException, IOException {
+        Credential credential = authorize();
         return new YouTube.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
                 JSON_FACTORY,
-                null)
+                credential)
                 .setApplicationName(APPLICATION_NAME)
-                .setYouTubeRequestInitializer(new YouTubeRequestInitializer(API_KEY))
                 .build();
+    }
+
+    private static Credential authorize() throws IOException {
+        InputStream in = Data.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = null;
+		try {
+			flow = new GoogleAuthorizationCodeFlow.Builder(
+			        GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, clientSecrets, SCOPES)
+			        .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
+			        .setAccessType("offline")
+			        .build();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (GeneralSecurityException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    private static String createPlaylist(YouTube youtubeService, String title, String description) throws IOException {
+        PlaylistSnippet playlistSnippet = new PlaylistSnippet();
+        playlistSnippet.setTitle(title);
+        playlistSnippet.setDescription(description);
+
+        PlaylistStatus playlistStatus = new PlaylistStatus();
+        playlistStatus.setPrivacyStatus("public");
+
+        Playlist youTubePlaylist = new Playlist();
+        youTubePlaylist.setSnippet(playlistSnippet);
+        youTubePlaylist.setStatus(playlistStatus);
+
+        YouTube.Playlists.Insert playlistInsertCommand = youtubeService.playlists().insert("snippet,status", youTubePlaylist);
+        Playlist playlistInserted = playlistInsertCommand.execute();
+        return playlistInserted.getId();
     }
 
     public static void main(String[] args) {
         List<List<Object>> videoData = processSearchWord("test");
         for (List<Object> video : videoData) {
             System.out.println("Title: " + video.get(0));
-            System.out.println("Duration: " + video.get(1));
+            System.out.println("Duration: " + video.get(1) + " seconds");
             System.out.println("View Count: " + video.get(2));
             System.out.println("Video ID: " + video.get(3));
             System.out.println();
         }
     }
 }
+
